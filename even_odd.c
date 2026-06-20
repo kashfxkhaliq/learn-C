802182236a60bf44411eb763629801498d647227
kashfxkhaliq <kashfxkhaliq@gmail.com>
2026-06-20 16:11:40 +0500

    Even Odd Numbers

---
+++ even_odd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+
+    printf("Enter The Number :: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0)
+    {
+        printf("%d is an Even Number", number);
+    }
+    
+    else
+    {
+        printf("%d is an Odd Number", number);
+    }
+}
