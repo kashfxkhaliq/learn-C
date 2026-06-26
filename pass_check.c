8c82ed5d796ff301a96c77eb724ff170e08b83e3
kashfxkhaliq <kashfxkhaliq@gmail.com>
2026-06-26 09:39:32 +0500

    Check Pass

---
+++ pass_check.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    float percentage;
+
+    printf("Enter The Percentage :: ");
+    scanf("%f", &percentage);
+
+    if (percentage > 50)
+    {
+       printf("PASS");
+    }
+    
+
+    
+}
